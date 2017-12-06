5cbf9bf9d4e337f85989544c0dbdae02bf6ea746
КимМ <@227_9>
2017-12-06 09:34:32 +0600

    Add project files.

---
+++ ConsoleApplication3/Source.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<locale.h>
+#include <iostream>
+#include<stdint.h>
+#include<math.h>
+using namespace std;
+char u;
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+/*	int x;
+	switch (x)
+	{
+		case 6:
+		{
+			cout << "x=6" << endl;
+		}
+		break;
+	default:
+	{
+
+	}
+	break;
+	}*/
+
+/*start:
+	int i = 0;
+	cout << "введите номер задания:";
+	cin >> i;
+	if (i == 1)
+	{
+
+	}*/
+}
